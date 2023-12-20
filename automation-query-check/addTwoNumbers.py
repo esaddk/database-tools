ed8e52e18fef2355c313edf0cadbbb8ab52b0f1c
esaddk <hello.esadewa@gmail.com>
2023-12-20 16:15:33 +0700

    cleansing unused file

--- automation-query-check/addTwoNumbers.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Please enter the first number: "))
-num2 = int(input("Please enter the second number: "))
-total = num1 + num2
-
-print("The value of the sum of two numbers is : ", total)
